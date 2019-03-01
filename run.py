0d53a1c932b170aa19c01df14903288aaae3a8f9
Jimmy Tuck <jim.tuck15@gmail.com>
2019-03-01 20:57:22 +0000

    appending messages

--- run.py
+++ run.py
@@ -1,12 +1,30 @@
-import os 
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1> Hello There! </h1>'
-    
-    
-app.run(host = os.getenv('IP'), port = int(os.getenv('PORT')), debug =True)
+messages = []
 
+def add_messages(username, message):
+    messages.append('{}: {}'.format(username, message))
+    
+
+@app.route("/")
+def index():
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
+
+
+@app.route("/<username>")
+def user(username):
+    """Display chat messages"""
+    return "Welcome, {0}".format(username, messages)
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    """Create a new messag and redirect back to chat page"""
+    return "{0}: {1}".format(username, message)
+
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
